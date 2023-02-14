96021f56af82de36656ab4251195941899c366c7
Tatiana Klimenko <tatiana.klimenko@stm-labs.ru>
2023-02-14 20:08:06 +0300

    Fix reflection usage

--- hw05-aop/src/main/java/homework/Ioc.java
+++ hw05-aop/src/main/java/homework/Ioc.java
@@ -4,6 +4,8 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Ioc {
 
@@ -18,6 +20,7 @@ public class Ioc {
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final TestLoggingInterface testLogging;
+        private final Set<Method> loggedMethods = new HashSet<>();
 
         DemoInvocationHandler(TestLoggingInterface testLogging) {
             this.testLogging = testLogging;
@@ -25,10 +28,21 @@ public class Ioc {
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (method.isAnnotationPresent(Log.class)) {
+            if (needLogging(method)) {
                 System.out.println("method: " + method.getName() + ", params: " + Arrays.toString(args));
             }
             return method.invoke(testLogging, args);
+        }
+
+        private boolean needLogging(Method method) {
+            if (loggedMethods.contains(method)) {
+                return true;
+            }
+            if (method.isAnnotationPresent(Log.class)) {
+                loggedMethods.add(method);
+                return true;
+            }
+            return false;
         }
     }
 }
