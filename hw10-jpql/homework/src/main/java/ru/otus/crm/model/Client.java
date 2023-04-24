6eba8a82a636ae78f5cfa60de73498981b0fb1ec
Tatiana Klimenko <taklimen@gmail.com>
2023-04-24 12:38:35 +0300

    HW10 - JPQL

---
+++ hw10-jpql/homework/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,106 @@
+package ru.otus.crm.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "client_gen", sequenceName = "client_seq",
+            initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Address address;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Phone> phones;
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(String name, Address address, List<Phone> phones) {
+        this(null, name, address, phones);
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        if (Objects.nonNull(address)) {
+            this.address.setClient(this);
+        }
+        this.phones = phones;
+        if (Objects.nonNull(phones)) {
+            this.phones.stream()
+                    .filter(Objects::nonNull)
+                    .forEach(phone -> phone.setClient(this));
+        }
+    }
+
+    @Override
+    public Client clone() {
+        var newAddress = Optional.ofNullable(this.address)
+                .map(Address::clone)
+                .orElse(null);
+
+        var newPhones = Objects.isNull(this.phones) ? null :
+                this.phones.stream()
+                        .map(phone -> Optional.ofNullable(phone)
+                                .map(Phone::clone)
+                                .orElse(null))
+                        .toList();
+        return new Client(this.id, this.name, newAddress, newPhones);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + nullableObjectToString(address) + '\'' +
+                ", phones='" + nullableListToString(phones) + '\'' +
+                '}';
+    }
+
+    private String nullableListToString(List<?> objects) {
+        if (Objects.isNull(objects)) {
+            return "null";
+        }
+        return objects.stream()
+                .map(this::nullableObjectToString)
+                .collect(Collectors.joining(" ,"));
+    }
+
+    private String nullableObjectToString(Object object) {
+        if (Objects.isNull(object)) {
+            return "null";
+        }
+        return object.toString();
+    }
+}
---
+++ hw10-jpql/homework/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,52 @@
+package ru.otus.crm.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "phone")
+public class Phone implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "phone_gen", sequenceName = "phone_seq",
+            initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Phone(String number) {
+        this.id = null;
+        this.number = number;
+    }
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    public Phone clone() {
+        return new Phone(this.id, this.number);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
